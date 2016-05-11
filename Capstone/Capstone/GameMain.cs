6895d1a00b9adddc76c83d78cd5c892da118f1da
aidandearing <aidan.dearing@live.com>
2016-05-10 23:23:54 -0400

    Fixed broken .csproj
    Fixed unnecessary Git metadata.

--- Capstone/Capstone/GameMain.cs
+++ Capstone/Capstone/GameMain.cs
@@ -112,7 +112,7 @@ namespace Capstone
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Aquamarine);
 
             // TODO: Add your drawing code here
             obj.Draw();
